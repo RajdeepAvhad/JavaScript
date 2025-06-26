a174cfd94bda59e32a4520796e9e26d7f5fa3dd5
RajdeepAvhad <150041450+RajdeepAvhad@users.noreply.github.com>
2025-06-26 19:53:53 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accId = 12345
+let accEmail = "raj@gmail.com"
+var accPass = "1114"
+
+accEmail = "ram@gmail.com"
+accPass = "7028"
+
+console.log(accEmail)
+
+console.table([accId , accEmail , accPass])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";   //treat all JS code as newer version
+
+// alert(3+3)  //we are using nodejs, not browser
+
+let name = "Raj"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standlone value
+// undefined => value not assign
+// symbol => unique
+
+// object 
+
+console.log(typeof "Raj")
+console.log(typeof undefined)
+console.log(typeof null)
